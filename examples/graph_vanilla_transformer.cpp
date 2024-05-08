f69b7f2d4ceb975a04bee91370b4cc00248ba7ea
dondavan <yeezushugh@gmail.com>
2024-05-08 12:55:03 +0200

    cleaning

--- examples/graph_vanilla_transformer.cpp
+++ examples/graph_vanilla_transformer.cpp
@@ -116,7 +116,7 @@ class GraphVanillaTransformerExample : public Example
                                 get_weights_accessor(data_path, "/positional_embedding.npy", operation_layout))
                      .set_name("tkemb1");
 
-        add_encoder_block(data_path," /layer_0" /*Layer Parameter Dir*/, d_model, h, eps, d_ff);
+        add_encoder_block(data_path,"/layer_0" /*Layer Parameter Dir*/, d_model, h, eps, d_ff);
 
         graph << OutputLayer(get_output_accessor(common_params)).set_name("out1");
 
